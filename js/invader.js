62d4ef0360b87d9629d79c877a9f816c74c99cae
Pedromartins74 <pedro.contawow@gmail.com>
2016-10-23 19:35:06 +0100

    Add files via upload

--- js/invader.js
+++ js/invader.js
@@ -4,39 +4,42 @@ var Invader = function(){
 	MovingObject.apply(this);
 
 	this._invader = this._movingObject;
+	this._invader.direction = CreateDir();
+
+	this._velocity = 0.5;
 
 	this.createInvader = function(x, y, z){
 		throw new Error('Cannot call abstract class \"Invader\" ');
 	}
 
-	this.move = function(){
-		dir = (RandomNum(0, 360)* Math.PI) / 180 ; //degrees->radians
-		if (0 < dir < 90){
-			this._invader.position.x += Math.cos(dir);
-			this._invader.position.y += Math.sin(dir);
+	this.move = function(dir){
+		var dir = this._invader.direction;
+		if (0 < dir < (Math.PI / 2)){
+			this._invader.position.x += Math.cos(dir)/2;
+			this._invader.position.y += Math.sin(dir)/2;
 		}
-		else if (90 < dir < 180){
-			this._invader.position.x -= Math.cos(dir);
-			this._invader.position.y += Math.sin(dir);
+		else if ((Math.PI / 2) < dir < Math.PI){
+			this._invader.position.x -= Math.cos(dir)/2;
+			this._invader.position.y += Math.sin(dir)/2;
 		}
-		else if (180 < dir < 270){
-			this._invader.position.x -= Math.cos(dir);
-			this._invader.position.y -= Math.sin(dir);
+		else if (Math.PI < dir < (3 * Math.PI)/2){
+			this._invader.position.x -= Math.cos(dir)/2;
+			this._invader.position.y -= Math.sin(dir)/2;
 		}
-		else if (270 < dir < 360){
-			this._invader.position.x += Math.cos(dir);
-			this._invader.position.y -= Math.sin(dir);
+		else if ((3 * Math.PI)/2 < dir < 2* Math.PI){
+			this._invader.position.x += Math.cos(dir)/2;
+			this._invader.position.y -= Math.sin(dir)/2;
 		}
 		else if (dir = 0){
 			this._invader.position.x += this._velocity;
 		}
-		else if (dir = 90){
+		else if (dir = Math.PI / 2){
 			this._invader.position.y += this._velocity;
 		}
-		else if (dir = 180){
+		else if (dir = Math.PI){
 			this._invader.position.x -= this._velocity;
 		}
-		else if (dir = 270){
+		else if (dir = (3*Math.PI)/2){
 			this._invader.position.y -= this._velocity;
 		}
 
@@ -46,5 +49,14 @@ var Invader = function(){
 	function RandomNum(min, max) {
   		return Math.random() * (max - min) + min;
 	}	
+
+	function CreateDir(){
+		dir = (RandomNum(0, 360)* Math.PI) / 180 ; //degrees->radians
+		return dir;
+	}
+
+	this.reverseDirection = function(){
+		this._invader.direction = -this._invader.direction;
+	}
 
 }
